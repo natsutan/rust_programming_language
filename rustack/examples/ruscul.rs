5775672609f42cb339617d1ed0c47d29c0b00604
natsutan <YRW03704@nifty.com>
2024-07-06 14:18:33 +0900

    4.6 終了

--- rustack/examples/ruscul.rs
+++ rustack/examples/ruscul.rs
@@ -1,50 +1,112 @@
 // https://github.com/msakuta/ruscal/tree/master/examples
+use nom::{
+  branch::alt,
+  bytes::complete::tag,
+  character::complete::{
+      alpha1, alphanumeric1, char, multispace0
+  },
+  combinator::recognize,
+  multi::{fold_many0, many0},
+  sequence::{delimited, pair},
+  IResult
+};
 
-#[derive(Debug, PartialEq, Eq)]
-enum Token {
-    Ident,
-    Number
+
+#[derive(Debug, PartialEq)]
+enum Token<'src> {
+    Ident(&'src str),
+    Number(f64),
+    LParen,
+    RParen
 }
 
-fn token(i :&str) -> (&str, Option<Token>) {
-    if let (i, Some(ident_res)) = ident(whitespace(i)) {
-        return (i, Some(ident_res));
-    }
-
-    if let (i, Some(number_res)) = number(whitespace(i)) {
-        return (i, Some(number_res));
-    }
-    (i, None)
+#[derive(Debug, PartialEq)]
+enum TokenTree<'src> {
+    Token(Token<'src>),
+    Tree(Vec<TokenTree<'src>>)
 }
 
-fn ident(mut input: &str) -> (&str, Option<Token>) {
-    if matches!(input.chars().next(), Some(_x @ ('a'..='z' | 'A'..='Z'))) {
-        while matches!(input.chars().next(), Some(_x @ ('a'..='z' | 'A'..='Z' | '0'..='9'))) {
-            let mut chars = input.chars();
-            chars.next();
-            input = chars.as_str();
-        }
-        (input, Some(Token::Ident))
+#[derive(Debug, PartialEq)]
+enum Expression<'src> {
+    Ident(&'src str),
+    NumLiteral(f64),
+    Add(Box<Expression<'src>>, Box<Expression<'src>>)
+}
+
+
+
+fn token(i :&str) -> Option<(&str, Token)> {
+    if let Some(res) = ident(whitespace(i)) {
+        return Some(res)
+    }
+    if let Some(number_res) = number(whitespace(i)) {
+        return Some(number_res);
+    }
+    if let Some(lparen_res) = lparen(whitespace(i)) {
+        return Some(lparen_res)
+    }
+    if let Some(rparen_res) = rparen(whitespace(i)) {
+        return Some(rparen_res)
+    }
+
+    None
+}
+
+fn advance_char(input: &str) -> &str {
+    let mut chars = input.chars();
+    chars.next();
+    chars.as_str()
+}
+
+fn peek_char(input: &str) -> Option<char> {
+    input.chars().next()
+}
+
+fn lparen(mut input: &str) -> (&str, Option<Token>) {
+    if matches!(peek_char(input), Some('(')) {
+        input = advance_char(input);
+        (input , Some(Token::LParen))
     } else {
         (input, None)
     }
 }
 
+fn rparen(mut input: &str) -> (&str, Option<Token>) {
+    if matches!(peek_char(input), Some(')')) {
+        input = advance_char(input);
+        (input , Some(Token::LParen))
+    } else {
+        (input, None)
+    }
+}
+
+
+fn ident(mut input: &str) -> Option<(&str, Token)> {
+    let start = input;
+    if matches!(peek_char(input), Some(_x @ ('a'..='z' | 'A'..='Z'))) {
+        input = advance_char(input);
+        while matches!(peek_char(input), Some(_x @ ('a'..='z' | 'A'..='Z' | '0'..='9'))) {
+            input = advance_char(input);
+        }
+        Some((input, Token::Ident(&start[..(start.len() - input.len())])))
+    } else {
+        None
+    }
+}
+
 fn whitespace(mut input: &str) -> &str {
-    while matches!(input.chars().next(), Some(' ')) {
-        let mut chars = input.chars();
-        chars.next();
-        input = chars.as_str();
+    while matches!(peek_char(input), Some(' ')) {
+        input = advance_char(input);
     }
     input
 }
 
 fn number(mut input: &str) -> (&str, Option<Token>) {
-    if matches!(input.chars().next(), Some(_x @ ('-' | '+' | '.' | '0' ..='9'))) {
-        while matches!(input.chars().next(), Some(_x @ ('.' | '0'..='9'))) {
-            let mut chars = input.chars();
-            chars.next();
-            input = chars.as_str();
+    if matches!(peek_char(input), Some(_x @ ('-' | '+' | '.' | '0' ..='9'))) {
+        input = advance_char(input);
+
+        while matches!(peek_char(input), Some(_x @ ('.' | '0'..='9'))) {
+            input = advance_char(input);
         }
         (input , Some(Token::Number))
     } else {
@@ -52,34 +114,116 @@ fn number(mut input: &str) -> (&str, Option<Token>) {
     }
 }
 
-fn source(mut input:& str) -> Vec<Token> {
+fn expr(input: &str) -> Option<(&str, Expression)> {
+    if let Some(res) = add(input) {
+        return Some(res);
+    }
+    if let Some(res) = term(input) {
+        return Some(res);
+    }
+    None
+}
+
+fn term(input: &str) -> Option<(&str, Expression)> {
+    if let Some(res) = paren(input) {
+        return Some(res);
+    }
+    if let Some(res) = token(input) {
+        return Some(res);
+    }
+    None
+}
+
+fn paren(input: &str) -> Option<(&str, Expression)> {
+    let next_input = lparen(whitespace(input))?;
+    let (next_input ,expr ) = expr(next_input)?;
+    let next_input = rparen(whitespace(next_input))?;
+
+    Some((next_input, expr))
+}
+
+fn add_term(input: &str) -> Option<(&str, Expression)> {
+    let (next_input, lhs) = term(input)?;
+    let next_input = plus(whitespace(next_input))?;
+
+    Some((next_input, lhs))
+}
+
+fn add(mut input: &str) -> Option<(&str, Expression)> {
+    let mut left = None;
+
+    while let Some((next_input, expr)) = add_term(input) {
+        if let Some(prev_left) = left {
+            left = Some(Expression::Add(
+                Box::new(prev_left),
+                Box::new(expr),
+            ));
+        } else {
+            left = Some(expr);
+        }
+        input = next_input;
+    }
+
+    let left = left?;
+
+    let (next_input, rhs) = expr(input)?;
+
+    Some((
+        next_input,
+        Expression::Add(Box::new(left), Box::new(rhs))
+    ))
+
+}
+
+fn plus(mut input: &str) -> Option<&str> {
+    if matches!(peek_char(input), Some('+')) {
+        Some(advance_char(input))
+    } else{
+        None
+    }
+}
+fn source(mut input:& str) -> (&str, TokenTree) {
     let mut tokens = vec![];
 
     while !input.is_empty() {
         input = if let (next_input, Some(token)) = token(input) {
-            tokens.push(token);
-            next_input
+            match token{
+                Token::LParen => {
+                    let (next_input, tt) = source(next_input);
+                    tokens.push(tt);
+                    next_input
+                }
+                Token::RParen => {
+                    return (next_input, TokenTree::Tree(tokens))
+                }
+                _ => {
+                    tokens.push(TokenTree::Token(token));
+                    next_input
+                }
+            }
+
+
         } else {
             break
         }
     }
 
 
-    tokens
+    (input, TokenTree::Tree(tokens))
 }
 
 fn main() {
-    let input = "123 world";
+    let input = "Hello World";
     println!(
         "source: {}, parsed: {:?}", input, source(input)
     );
 
-    let input = "Hello world";
+    let input = "(123 456 ) world";
     println!(
         "source: {}, parsed: {:?}", input, source(input)
     );
 
-    let input = "    world";
+    let input = "((car cdr) cdr)";
     println!(
         "source: {}, parsed: {:?}", input, source(input)
     );
---
+++ rustack/examples/ruscul_nom.rs
@@ -0,0 +1,239 @@
+// https://github.com/msakuta/ruscal/tree/master/examples
+
+use std::collections::HashMap;
+use std::io::Read;
+
+use nom::{branch::alt, bytes::complete::tag, character::complete::{
+    alpha1, alphanumeric1, char, multispace0
+}, combinator::{opt, recognize}, multi::{fold_many0, many0}, sequence::{delimited, pair}, IResult, Parser, error::ParseError, Finish};
+use nom::multi::separated_list0;
+use nom::number::complete::recognize_float;
+use nom::sequence::terminated;
+
+fn space_delimited<'src, O, E>(f: impl Parser<&'src str, O, E>)
+    -> impl FnMut(&'src str) -> IResult<&'src str, O, E> where E: ParseError<&'src str>,
+{
+    delimited(multispace0, f, multispace0)
+}
+
+fn main() {
+    let mut buf = String::new();
+
+    if !std::io::stdin().read_to_string(&mut buf).is_ok() {
+        panic!("Failed to read from stdin.");
+    }
+
+    let parsed_statements = match statements(&buf) {
+        Ok(parsed_statements) => parsed_statements,
+        Err(e) => {
+            eprintln!("Parse error {e:?}");
+            return;
+        }
+    };
+
+    let mut variables = HashMap::new();
+
+    for statement in parsed_statements {
+        match statement {
+            Statement::Expression(expr) => {
+                println!("eval: {:?}", eval(expr, &variables));
+
+            }
+            Statement::VarDef(name,expr) => {
+                let value = eval(expr, &variables);
+                variables.insert(name, value);
+            }
+            Statement::VarAssign(name, expr) => {
+                if !variables.contains_key(name) {
+                    panic!("Variable is not defined.");
+                }
+                let value = eval(expr, &variables);
+                variables.insert(name, value);
+            }
+
+        }
+    }
+
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Expression<'src> {
+    Ident(&'src str),
+    NumLiteral(f64),
+    FnInvoke(&'src  str, Vec<Expression<'src>>),
+    Add(Box<Expression<'src>>, Box<Expression<'src>>),
+    Sub(Box<Expression<'src>>, Box<Expression<'src>>),
+    Mul(Box<Expression<'src>>, Box<Expression<'src>>),
+    Div(Box<Expression<'src>>, Box<Expression<'src>>)
+
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Statement<'src> {
+    Expression(Expression<'src>),
+    VarDef(&'src str, Expression<'src>),
+    VarAssign(&'src str, Expression<'src>),
+}
+
+
+type Statements<'a> = Vec<Statement<'a>>;
+
+fn unary_fn(f: fn(f64)->f64) -> impl Fn(Vec<Expression>, &HashMap<&str, f64>) -> f64 {
+    move |args, variables| {
+        f(eval(
+            args.into_iter().next().expect("function missing argument"),
+            variables,
+        ))
+    }
+}
+
+fn binary_fn(f: fn(f64, f64) -> f64,) -> impl Fn(Vec<Expression>, &HashMap<&str, f64>) -> f64 {
+    move |args, variables| {
+        let mut args = args.into_iter();
+        let lhs = eval(args.next().expect("function missing first argument"),variables);
+        let rhs = eval(args.next().expect("function missing second argument"), variables);
+        f(lhs, rhs)
+    }
+
+}
+
+fn eval(expr: Expression, vars: &HashMap<&str, f64>) -> f64 {
+    use Expression::*;
+    match expr {
+        Ident("pi") => std::f64::consts::PI,
+        Ident(id) => *vars.get(id).expect("Variable not found"),
+        NumLiteral(n) => n,
+        FnInvoke("sqrt", args) => unary_fn(f64::sqrt)(args, vars),
+        FnInvoke("sin", args) => unary_fn(f64::sin)(args, vars),
+        FnInvoke("cos", args) => unary_fn(f64::cos)(args, vars),
+        FnInvoke("tan", args) => unary_fn(f64::tan)(args, vars),
+        FnInvoke("asin", args) => unary_fn(f64::asin)(args, vars),
+        FnInvoke("acos", args) => unary_fn(f64::acos)(args, vars),
+        FnInvoke("atan", args) => unary_fn(f64::atan)(args, vars),
+        FnInvoke("atan2", args) => binary_fn(f64::atan2)(args, vars),
+        FnInvoke("pow", args) => binary_fn(f64::powf)(args, vars),
+        FnInvoke("exp", args) => unary_fn(f64::exp)(args, vars),
+        FnInvoke("log", args) => binary_fn(f64::log)(args, vars),
+        FnInvoke("log10", args) => unary_fn(f64::log10)(args, vars),
+        FnInvoke(name, _) => {
+            panic!("Unknown function {name:?}")
+        }
+        Add(lhs, rhs) => eval(*lhs, vars) + eval(*rhs, vars),
+        Sub(lhs, rhs) => eval(*lhs, vars) - eval(*rhs, vars),
+        Mul(lhs, rhs) => eval(*lhs, vars) * eval(*rhs, vars),
+        Div(lhs, rhs) => eval(*lhs, vars) / eval(*rhs, vars)
+    }
+}
+
+fn expr(i: &str) -> IResult<&str, Expression> {
+    let (i, init) = term(i)?;
+
+    fold_many0(
+        pair(space_delimited(alt((char('+'), char('-')))), term),
+        move || init.clone(),
+        | acc, (op, val): (char, Expression) |
+            match op {
+                '+' => Expression::Add(Box::new(acc), Box::new(val)),
+                '-' => Expression::Sub(Box::new(acc), Box::new(val)),
+                _ => panic!(
+                    "Additive expression should have + or - operator"
+                ),
+        }
+    )(i)
+}
+
+fn term(i: &str) -> IResult<&str, Expression> {
+    let (i, init) = factor(i)?;
+
+    fold_many0(
+        pair(space_delimited(alt((char('*'), char('/')))), factor),
+        move || init.clone(), | acc, (op, val): (char, Expression) |
+        match op {
+            '*' => Expression::Mul(Box::new(acc), Box::new(val)),
+            '/' => Expression::Div(Box::new(acc), Box::new(val)),
+            _ => panic!(
+                "Multiplicative expression should have * or / operator"
+            ),
+
+        }
+    )(i)
+}
+
+fn factor(i: &str) -> IResult<&str, Expression> {
+    alt((number, func_call, ident, parens))(i)
+}
+
+fn func_call(i: &str) -> IResult<&str, Expression> {
+    let (r, ident) = space_delimited(identifier)(i)?;
+    let (r, args) = space_delimited(delimited(
+        tag("("),
+        many0(delimited(multispace0, expr, space_delimited(opt(tag(","))),)),
+        tag(")"),
+    ))(r)?;
+    Ok((r, Expression::FnInvoke(ident, args)))
+}
+
+
+fn ident(input: &str) -> IResult<&str, Expression> {
+    let (r, res) =
+        delimited(multispace0, identifier, multispace0)(input)?;
+    Ok((r, Expression::Ident(res)))
+}
+
+fn identifier (input: &str ) -> IResult<&str, &str> {
+    recognize(pair(
+        alt((alpha1, tag("_"))),
+        many0(alt((alphanumeric1, tag("_")))),
+    ))(input)
+}
+
+fn number(input: &str) -> IResult<&str, Expression> {
+    let (r, v) =
+        delimited(multispace0, recognize_float, multispace0)(input, )?;
+
+    Ok((
+        r,
+        Expression::NumLiteral(v.parse().map_err(|_| {
+            nom::Err::Error(nom::error::Error {
+                input,
+                code: nom::error::ErrorKind::Digit,
+            })
+        })?),
+    ))
+}
+
+fn parens(i: &str) -> IResult<&str, Expression> {
+    delimited(
+       multispace0,
+       delimited(tag("("), expr, tag(")")),
+       multispace0
+    )(i)
+}
+
+fn statement(i: &str) -> IResult<&str, Statement> {
+    alt((var_def, var_assign, expr_statement))(i)
+}
+
+fn var_def(i: &str) -> IResult<&str, Statement> {
+    let (i0, _) = space_delimited(tag("var"))(i)?;
+    let (i1, name) = space_delimited(identifier)(i0)?;
+    let (i2, _) = space_delimited(char('='))(i1)?;
+    let (i3, expr) = space_delimited(expr)(i2)?;
+    Ok((i3, Statement::VarDef(name, expr)))
+}
+
+fn var_assign(i: &str) -> IResult<&str, Statement> {
+    let(i, name) = space_delimited(identifier)(i)?;
+    let(i, _) = space_delimited(char('='))(i)?;
+    let(i, expr) = space_delimited(expr)(i)?;
+    Ok((i, Statement::VarAssign(name, expr)))
+}
+
+fn expr_statement(i: &str) -> IResult<&str, Statement> {
+    let (i, res) = expr(i)?;
+    Ok((i, Statement::Expression(res)))
+}
+fn statements(i: &str) -> Result<Statements, nom::error::Error<&str>> {
+    let (_, res) = separated_list0(tag(";"), statement)(i).finish()?;
+    Ok(res)
+}
